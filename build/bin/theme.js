e63e81bafa7142c97e9619894209dc8d8e5c7cdb
xiehui <30897863@qq.com>
2018-11-20 19:43:00 +0800

    提取 生成 样式文件的类型变量 css_type

--- build/bin/theme.js
+++ build/bin/theme.js
@@ -1,4 +1,5 @@
 'use strict';
+const css_type='scss'
 const chalk = require('chalk')
 /**生成组件css文件**/
 console.log('');
@@ -22,9 +23,9 @@ const themePath = dir(`themes`);
 const tplGulp = fs.readFileSync(dir('build/tpl/theme.gulp.tpl'), 'utf8');
 const tplPackage = fs.readFileSync(dir('build/tpl/theme.package.tpl'), 'utf8');
 //import代码块模板
-const IMPORT_TEMPLATE = '@import \'./{{name}}.scss\';';
-const importScss = [];
-const indexPath = dir(`themes/${themeName}/src/index.scss`);
+const IMPORT_TEMPLATE = `@import './{{name}}.${css_type}';`;
+const importCss = [];
+const indexPath = dir(`themes/${themeName}/src/index.${css_type}`);
 //跨平台行末标识符
 const endOfLine = require('os').EOL;
 
@@ -58,15 +59,15 @@ if (!fs.existsSync(gulpPath)) {
 //根据组件生成主题文件
 let comsDir = fs.readdirSync(comsPath);
 comsDir.forEach(file => {
-  let filePath = dir(`${themePath}/${themeName}/src/${file}.scss`);
+  let filePath = dir(`${themePath}/${themeName}/src/${file}.${css_type}`);
   if (!fs.existsSync(filePath)) {
     fileSave(filePath);
   }
-  importScss.push(render(IMPORT_TEMPLATE, {name: file}));
+  importCss.push(render(IMPORT_TEMPLATE, {name: file}));
 });
 //生成index.scss
 fileSave(indexPath)
-  .write(importScss.join(endOfLine),'utf8')
+  .write(importCss.join(endOfLine),'utf8')
   .end('\n');
 //生成fonts文件夹
 let fontsPath = dir(`${themePath}/${themeName}/src/fonts`);
